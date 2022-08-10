d7f7e1e1c49a7ce5181cd15f458ee6cb5f19600f
fitorhu <fitorhu@zego.im>
2022-08-10 15:24:26 +0800

    🎨 添加导航栏配置

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    title: 'somebody study文档',
+    description: 'vue,typescript,nuxt',
+    themeConfig:{
+        // 官方文档：https://vuepress.vuejs.org/zh/theme/default-theme-config.html#%E5%AF%BC%E8%88%AA%E6%A0%8F
+        nav:[
+            {text:'首页',link:'/'},
+            {
+                text:'八妹 的前端乱七八糟博客',
+                items: [
+                    { text: '前端', link: 'https://github.com/huyarufitor' },
+                    { text: '后端', link: 'https://juejin.cn/user/712139234359182/posts' },
+                    { text: '服务器', link: 'https://github.com/huyarufitor' },
+                    { text: '数据库', link: 'https://github.com/huyarufitor' },
+                    { text: 'WEB安全+HTTP', link: 'https://github.com/huyarufitor' },
+                    { text: '408', link: 'https://github.com/huyarufitor' },
+                    { text: 'web 八股文', link: 'https://github.com/huyarufitor' },
+                    
+                ]
+            }
+        ]
+    }
+  }
