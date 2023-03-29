a926e29cca36c3efd985b7ba09e18c261543a211
fitorhu <fitorhu@zego.im>
2023-03-29 11:49:49 +0800

    fix：从后往前merge到数组nums1中

--- docs/javascript/leetcode/88-有序数组.js
+++ docs/javascript/leetcode/88-有序数组.js
@@ -1,8 +1,30 @@
-let nums1 = [1,2,3,0,0,0];
-let m = 3;
-let nums2 = [2,5,6];
+let nums1 = [0,0,0];
+let m = 0;
+let nums2 = [2,2,3];
 let n = 3;
 var merge = function(nums1, m, nums2, n) {
-    let p = 0;//p指针指向nums1的头，q指针指向nums2的头
-    let q = 0 ;
+    let i = m - 1;// 标识nums1的下标
+    let j = n - 1;//标识nums2的下标
+    let k = m + n - 1;
+    while (i >= 0 && j >= 0) {
+        //处理两个数组都有值的时候合并
+        if (nums1[i] > nums2[j]) {
+            nums1[k] = nums1[i];
+            i--;
+            console.log('1');
+        } else {
+            nums1[k] = nums2[j];
+            j--;
+            console.log('2');
+        }
+        k--;
+    }
+    //处理nums1数组没有有效元素，nums2数组不为空的情况
+    while(j>=0){
+        nums1[k] = nums2[j];
+        j--;
+        k--;
+    }
+    console.log('nums1',nums1);
 };
+merge(nums1,m,nums2,n);
