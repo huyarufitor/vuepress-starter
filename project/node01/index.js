d97bb129cf97674dd0594188d93d0fd479e280b1
fitorhu <fitorhu@zego.im>
2023-06-20 19:24:14 +0800

    feat:express 初体验

--- project/node01/index.js
+++ project/node01/index.js
@@ -3,7 +3,12 @@ const app = express()
 const port = 1010
 app.get('/',(req,res)=>{
     res.send('Hello world!')
+})  
+app.post('/',(req,res)=>{
+    res.send('Got a POST request!')
 })
 app.listen(port,()=>{
     console.log(`Example has already runned on ${port}`);
 })
+app.use(express.static('public'))
+app.use('/static', express.static('public'))
