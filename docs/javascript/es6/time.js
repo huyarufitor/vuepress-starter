1580a0a3b420b016ce569808e800772348deb048
fitorhu <fitorhu@zego.im>
2023-09-05 19:44:15 +0800

    fix

--- docs/javascript/es6/time.js
+++ docs/javascript/es6/time.js
@@ -7,7 +7,7 @@ function calculateDuration(start, end) {
     const durationInHours = (durationInMs / (1000 * 60 * 60));
     return durationInHours.toFixed(1)-1.5;
   }
-  console.log(calculateDuration('09:45','20:20'))
+  console.log(calculateDuration('09:55','19:10'))
 /**09:56,19:52 --8.4
  09:57,19:20 --7.9
 09:51,19:51 --8.5
@@ -25,19 +25,21 @@ function calculateDuration(start, end) {
 调休假(全天)
  09:56,19:33 --8.1
 09:56,18:59 --7.6
-09:56,19:08 
+09:56,19:08 --7.7
 09:56,20:43 --9.3
-09:30,19:48 
-09:57,19:47 
-09:58,19:10 
-09:53,19:05 
-09:55,19:10 */
-//--8.4
+09:30,19:48 --8.8
+09:57,19:47 --8.3
+09:58,19:10 --7.7
+09:53,19:05 --7.7
+09:55,19:10 --7.8
+*/
+//----22d 181.5 平均8.25
+//
   //
 //   console.log(calculateDuration('09:57','19:20'))
   //--7.9
 //   console.log(calculateDuration('09:56','19:52'))--8.4
 //   console.log(calculateDuration('09:56','19:52'))--8.4
 
-9.45-20:20 ---9.1
+// 9.45-20:20 ---9.1
   
